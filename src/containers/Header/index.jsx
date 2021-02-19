e5482cce5f1a1ff76cf274ae7703d9e4c52fb5e3
MilsonCodes <mshepp8@lsu.edu>
2021-02-19 14:28:34 -0600

    initial file structure (separate components)

---
+++ src/containers/Header/index.jsx
@@ -0,0 +1,13 @@
+import { Typography, Box } from '@material-ui/core';
+import React from 'react';
+
+const Header = () => {
+    return (
+        <Box>
+          <Typography variant="h2">ArcViz Alpha</Typography>
+          <Typography variant="subtitle1">Interactive, Arcitecture Visualization Tool</Typography>
+        </Box>
+    );
+}
+
+export default Header;
